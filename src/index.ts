61148cdb9f8324d8190c58bc667d075bee31495f
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-06-24 16:49:29 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
 // async function insertData() {
 //   const client = new Client({
 //     connectionString:
-//       "postgresql://neondb_owner:xgdFNrXR7I3M@ep-bitter-feather-a5bvattm.us-east-2.aws.neon.tech/neondb?sslmode=require",
+//       "",
 //   });
 //   await client.connect();
 //   try {
